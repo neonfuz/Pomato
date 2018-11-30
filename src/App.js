183e8537ca4e7a3365fc9093f73c061412304a5e
Sage Raflik <neonfuz@gmail.com>
2018-11-30 12:32:44 -0800

    Made timer increment every minute, added service worker for offline

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Favicon from './favicon.js'
 const App = () => (
   <div className="App">
     <h1> Favicon Timer! </h1>
-    <Timer interval={1000}>{([time, reset]) => (
+    <Timer interval={1000 * 60}>{([time, reset]) => (
       <>
         <Favicon number={time} />
         <button onClick={reset}>Reset</button>

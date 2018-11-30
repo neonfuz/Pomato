a85be8f492a754c7266a35c1c950dbbd9d5f0e98
Sage Raflik <neonfuz@gmail.com>
2018-11-30 02:24:28 -0800

    Converted Favicon to class Component

--- src/favicon.js
+++ src/favicon.js
@@ -1,32 +1,25 @@
-import { useMemo, useEffect } from 'react'
+import { Component } from 'react'
 
-class FaviconRenderer {
-  constructor() {
+class Favicon extends Component {
+  constructor(props) {
+    super(props)
     this.canvas = document.createElement('canvas')
     this.canvas.width = this.canvas.height = 16
     this.ctx = this.canvas.getContext("2d")
     this.ctx.font = "12px Arial"
     this.favicon = document.querySelector("link[href='/favicon.ico']")
   }
-  render(number, color='black', bg='white') {
+  componentDidMount() { this.renderFavicon() }
+  componentDidUpdate() { this.renderFavicon() }
+  renderFavicon() {
+    const {number, color = 'black', bg = 'white'} = this.props
     this.ctx.fillStyle = bg
     this.ctx.fillRect(0, 0, 16, 16)
     this.ctx.fillStyle = color
     this.ctx.fillText(number, 1, 12)
     this.favicon.href = this.canvas.toDataURL('image/png')
   }
-}
-
-const ONCE = []
-
-const Favicon = ({ number, color='black', bg='white' }) => {
-  const renderer = useMemo(() => new FaviconRenderer(), ONCE)
-  useEffect(() => {
-    renderer.render(number, color, bg)
-  }, [number, color, bg])
-
-  // Render nothing to the dom
-  return null
+  render() { return null } // Render nothing to the dom
 }
 
 export default Favicon

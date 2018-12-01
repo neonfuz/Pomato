cc32a019b3a3e4eae3be7899696c35d9af19d4f7
Sage Raflik <neonfuz@gmail.com>
2018-12-01 13:06:26 -0800

    favicon: Exposed font weight and offsets as props

--- src/favicon.js
+++ src/favicon.js
@@ -6,17 +6,22 @@ class Favicon extends Component {
     this.canvas = document.createElement('canvas')
     this.canvas.width = this.canvas.height = 16
     this.ctx = this.canvas.getContext("2d")
-    this.ctx.font = "12px Arial"
     this.favicon = document.querySelector("link[rel$='icon']")
   }
   componentDidMount() { this.renderFavicon() }
   componentDidUpdate() { this.renderFavicon() }
   renderFavicon() {
-    const {number, color = 'black', bg = 'white'} = this.props
+    const {
+      number,
+      color = 'black', bg = 'white',
+      weight = 'normal',
+      off1 = 7, off2 = 0,
+    } = this.props
     this.ctx.fillStyle = bg
     this.ctx.fillRect(0, 0, 16, 16)
     this.ctx.fillStyle = color
-    const offset = number < 10 ? 7 : 0
+    const offset = number < 10 ? off1 : off2
+    this.ctx.font = `${weight} 12px Arial`
     this.ctx.fillText(number, 1 + offset, 12)
     this.favicon.href = this.canvas.toDataURL('image/png')
   }
